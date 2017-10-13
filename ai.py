1b10229d54641f8e31e7a5d8f84c0e7f41026906
ALexandru Mortan <mortanalexandru@yahoo.com>
2017-10-13 16:56:14 +0100

    new features

--- ai.py
+++ ai.py
@@ -1,6 +1,6 @@
 # import copy
 import numpy as np
-
+import math
 
 class AI:
     def __init__(self, heightWeight, linesWeight, holesWeight, bumpinessWeight):
@@ -31,11 +31,17 @@ class AI:
 
     def get_reward(self, game):
         heights = np.zeros(10)
+        max_height = 0
+        min_height = 0
         holes = 0
         for col in range(10):
             column = game.grid[:, col]
             nonzeros = np.flatnonzero(column)
             heights[col] = (20 - nonzeros[0]) if len(nonzeros) > 0 else 0
+            if heights[col] > max_height:
+                max_height = heights[col]
+            if heights[col] < min_height:
+                min_height = heights[col]
             ceiling = False
             for i in column:
                 if i == 0 and ceiling:
@@ -44,6 +50,8 @@ class AI:
                 if i > 0:
                     ceiling = True
 
+        weighted_height = math.pow(max_height, 1.5)
+        relative_height = max_height - min_height
         agg_height = np.sum(heights)
         lines = 0
         for row in game.grid:
@@ -53,5 +61,8 @@ class AI:
         bumpiness = 0
         for col in range(9):
             bumpiness += abs(heights[col] - heights[col + 1])
-        reward = -self.heightWeight * agg_height + self.linesWeight * lines - self.holesWeight * holes - self.bumpinessWeight * bumpiness
+        # reward = -self.heightWeight * agg_height + self.linesWeight * lines - self.holesWeight * holes - self.bumpinessWeight * bumpiness
+
+        reward = 0.22568649650722883 * lines - 0.08679520494876472 * weighted_height - 0.6152727732730796 * agg_height + 0.15842464424735841 *relative_height -0.15452215909537684 * holes - 0.021586109522043928 * bumpiness
+
         return reward
